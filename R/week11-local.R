5ed4ee86b04aebe92855ca54dea6f42f318c7620
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-04-06 15:19:48 -0500

    Part 2 - Objective 6

--- R/week11-local.R
+++ R/week11-local.R
@@ -6,26 +6,18 @@ library(caret)
 set.seed(8712)
 
 #### Data Import and Cleaning #### 
-# Import SPSS data (Road haven package)
 gss_original_tbl <- read_sav(file = "../data/GSS2016.sav")
 
 # Create a variable gss_tbl
 gss_tbl <- gss_original_tbl %>%
-  # Remove rows where MOSTHRS is missing by using filter and !is.na
   filter(!is.na(MOSTHRS)) %>%
-  # Rename MOSTHRS to workhours by using rename
   rename(workhours = MOSTHRS) %>%
-  # Remove HRS1 and HRS2 variables by using select 
   select(-c(HRS1, HRS2)) %>%
-  # Retain only variables with less than 75% missingness by using select
   select(where(~mean(is.na(.)) < 0.75)) %>%
-  # Convert to data to numeric variables
   sapply(as.numeric) %>% 
-  # Convert to data frame to ensure compatibility with dplyr functions by using as.data.frame
   as_tibble()  
 
 #### Visualization #### 
-# Making the histogram
 ggplot(gss_tbl, aes(x = workhours)) +
   geom_histogram() +
   labs(x = "Working hours",
@@ -33,103 +25,116 @@ ggplot(gss_tbl, aes(x = workhours)) +
        title = "Histogram of working hours")
 
 #### Analysis #### 
-## Prepare data set to run ML
-# Create sample rows  
-gss_sample <- sample(nrow(gss_tbl))
-# Shuffle using the sampled indices 
-gss_shuffled <- gss_tbl[gss_sample,]
-# Calculate the number of rows for a 75/25 split
-gss_75per <- round(nrow(gss_shuffled) * 0.75)
-# Create the training set using the first 75% of the shuffled data
-gss_train <- gss_shuffled[1:gss_75per,]
-# Create the test set using the remaining 25% of the shuffled data
-gss_test <- gss_shuffled[(gss_75per + 1):nrow(gss_shuffled), ]
-# Create 10 folds for cross-validation using the workhours column from the training set
-gss_folds <- createFolds(gss_train$workhours, 10)
-# Set up train control for all model
-train_control <- trainControl(method = "cv", 
-                              number = 10, 
-                              index = gss_folds, 
-                              verboseIter = TRUE)
+# I fixed the week 10 based on debrief video and code to improve Project 11. I added comments to indicate changes made.
 
-## Run OLS,  elastic net, random forest, and eXtreme Gradient Boosting
-# Train the OLS regression model using train
+# Create holdout indices for data partitioning
+holdout_indices <- createDataPartition(gss_tbl$workhours,
+                                       p = .25,
+                                       list = T)$Resample1
+# Create a test set using the holdout indices
+test_tbl <- gss_tbl[holdout_indices,]
+# Create a training set by excluding the holdout indices
+training_tbl <- gss_tbl[-holdout_indices,]
+# Create cross-validation folds for the training data
+training_folds <- createFolds(training_tbl$workhours)
+
+## OLS Model
+# Train the OLS regression model using train function
 model_OLS <- train(
   workhours ~ .,
-  data = gss_train,
-  method = "lm", 
-  metric = "Rsquared",
-  preProcess = "medianImpute",
+  training_tbl,
+  method="lm",
   na.action = na.pass,
-  trControl = train_control
-  )
+  preProcess = c("center","scale","zv","nzv","medianImpute"),
+  trControl = trainControl(method="cv", 
+                           number=10, 
+                           verboseIter=T, 
+                           indexOut = training_folds)
+)
 
-OLS_predict <- predict(model_OLS, gss_test, na.action = na.pass)
+# Extracting cross-validation results (R-squared) from the OLS model
+cv_OLS <- model_OLS$results$Rsquared
 
+# Evaluating the OLS model on the holdout test set
+holdout_OLS <- cor(
+  predict(model_OLS, test_tbl, na.action = na.pass),
+  test_tbl$workhours
+)^2
+
+## Elastic net model
 # Train the elastic net model using train
 model_elastic <- train(
   workhours ~ .,
-  data = gss_train,
-  method = "glmnet", 
-  metric = "Rsquared",
+  training_tbl,
+  method="glmnet",
   na.action = na.pass,
-  preProcess = "medianImpute",
-  trControl = train_control
-  )
+  preProcess = c("center","scale","zv","nzv","medianImpute"),
+  trControl = trainControl(method="cv", 
+                           number=10, 
+                           verboseIter=T, 
+                           indexOut = training_folds)
+)
+# Extracting cross-validation results (R-squared) from the Elastic net model
+cv_elastic <- max(model_elastic$results$Rsquared)
+# Evaluating the Elastic net model on the holdout test set
+holdout_elastic <- cor(
+  predict(model_elastic, test_tbl, na.action = na.pass),
+  test_tbl$workhours
+)^2
 
+## Random forest model
 # Train the random forest model using train
 model_random <- train(
   workhours ~ .,
-  data = gss_train,
-  method = "ranger", 
-  metric = "Rsquared",
+  training_tbl,
+  method="ranger",
   na.action = na.pass,
-  preProcess = "medianImpute",
-  trControl = train_control
+  preProcess = c("center","scale","zv","nzv","medianImpute"),
+  trControl = trainControl(method="cv", 
+                           number=10, 
+                           verboseIter=T, 
+                           indexOut = training_folds)
 )
+# Extracting cross-validation results (R-squared) from the random forest model
+cv_random <- max(model_random$results$Rsquared)
+# Evaluating the random forest model on the holdout test set
+holdout_random <- cor(
+  predict(model_random, test_tbl, na.action = na.pass),
+  test_tbl$workhours
+)^2
 
+## Random XGB
 # Train the random XGB using train
 model_XGB <- train(
   workhours ~ .,
-  data = gss_train,
-  method = "xgbLinear", 
-  metric = "Rsquared",
+  training_tbl,
+  method="xgbLinear",
   na.action = na.pass,
-  preProcess = "medianImpute",
-  trControl = train_control
+  tuneLength = 1,
+  preProcess = c("center","scale","zv","nzv","medianImpute"),
+  trControl = trainControl(method="cv", 
+                           number=10, 
+                           verboseIter=T, 
+                           indexOut = training_folds)
 )
+# Extracting cross-validation results (R-squared) from the random XGB
+cv_XGB <- max(model_XGB$results$Rsquared)
+# Evaluating the random XGB on the holdout test set
+holdout_XGB <- cor(
+  predict(model_XGB, test_tbl, na.action = na.pass),
+  test_tbl$workhours
+)^2
+
+## Summarize and Visualization
+# Calculating the summary of resampling results for multiple models
+summary(resamples(list(model_OLS, model_elastic, model_random, model_XGB)), metric="Rsquared")
+# Visualizing the resampling results for multiple models using a dotplot
+dotplot(resamples(list(model_OLS, model_elastic, model_random, model_XGB)), metric="Rsquared")
 
 #### Publication #### 
-# Make predictions using the models
-OLS_predict <- predict(model_OLS, gss_test, na.action = na.pass)
-elastic_predict <- predict(model_elastic, gss_test, na.action = na.pass)
-random_predict <- predict(model_random, gss_test, na.action = na.pass)
-XGB_predict <- predict(model_XGB, gss_test, na.action = na.pass)
 
-# Calculate R-squared values for holdout CV
-ho_rsq_OLS <- cor(OLS_predict, gss_test$workhours)^2
-ho_rsq_elastic <- cor(elastic_predict, gss_test$workhours)^2
-ho_rsq_random <- cor(random_predict, gss_test$workhours)^2
-ho_rsq_XGB <- cor(XGB_predict, gss_test$workhours)^2
 
-# Create a tibble with the desired structure
-table1_tbl <- tibble(
-  algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"),
-  cv_rsq = c(
-    str_remove(format(round(model_OLS$results$Rsquared, 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(max(model_elastic$results$Rsquared), 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(max(model_random$results$Rsquared), 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(max(model_XGB$results$Rsquared), 2), nsmall = 2), pattern = "^0")),
-  ho_rsq = c(
-    str_remove(format(round(ho_rsq_OLS, 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(ho_rsq_elastic, 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(ho_rsq_random, 2), nsmall = 2), pattern = "^0"),
-    str_remove(format(round(ho_rsq_XGB, 2), nsmall = 2), pattern = "^0")
-  )
-)
 
-# Print the table
-print(table1_tbl)
 
 # A1. 
 # The results, measured in R-squared values, varied significantly across the different models. 
